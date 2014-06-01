f324e28cfbff4f0be2e357f80815cb7f55d973d0
Richard McIntyre <richard.mackstar@gmail.com>
2014-06-01 21:43:09 +0100

    WIP

---
+++ src/autocomplete.js
@@ -0,0 +1,28 @@
+angular.module('mackstar.autocomplete', [])
+
+.directive('autoCompleteOptions', function() {
+  return {
+    restrict:'EA',
+    replace: true,
+      template: '<ul class="dropdown-menu"><li ng-repeat="option in options"><a>{{}}</a></li></ul>'
+  }
+})
+
+.directive('autoComplete', function($compile) {
+  return {
+    restrict:'EA',
+    transclude:false,
+    replace: false,
+      link: function (scope, element) {
+          var el = $compile( "<div auto-complete-options></div>" )( scope );
+          element.parent().append(el);
+          scope.$apply();
+      }
+  }
+});
+
+
+.module("template/autocomplete.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("template/autocomplete.html",
+    "<div>Autocomplete</div>");
+}]);
